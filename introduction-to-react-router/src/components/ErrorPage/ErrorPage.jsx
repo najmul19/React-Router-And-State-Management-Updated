d536cadec32f91c275387b1f7c927ffc99f4d4d5
najmul <mdnajmulislam10992@gmail.com>
2025-04-07 01:38:37 +0600

    useRouteError

--- introduction-to-react-router/src/components/ErrorPage/ErrorPage.jsx
+++ introduction-to-react-router/src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,20 @@
+import { Link, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+    const error = useRouteError();
+    // console.log(error)
     return (
         <div>
             <h2>Oppss!!!</h2>
+            <p>{error.statusText || error.message}</p>
+            {
+                error.status  === 404 && <div>
+                    <h3>Page Not Found</h3>
+                    <p>Go back Where You from</p>
+                    <Link to='/'><button>Home</button></Link>
+                </div>
+            }
         </div>
     );
 };
