8750e971034da789333eea613ae49372148d2fcf
najmul <mdnajmulislam10992@gmail.com>
2025-04-06 23:24:09 +0600

    nasted rout concept and outlet

---
+++ introduction-to-react-router/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>Home components</h2>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Home;
