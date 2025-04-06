e5d1dcbd6c3f830df831963746dda6b2409f0584
najmul <mdnajmulislam10992@gmail.com>
2025-04-07 01:49:23 +0600

    active route use NavLink

--- introduction-to-react-router/src/components/Header/Header.jsx
+++ introduction-to-react-router/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
   return (
@@ -8,7 +8,10 @@ const Header = () => {
         <Link to="/" >Home</Link>
         <Link to="/about" >About</Link>
         <Link to="/contact" >Contact Us</Link>
-        <Link to="/users" >Users</Link>
+        <NavLink to="/users">
+            Users
+        </NavLink>
+        {/* <Link to="/users" >Users</Link> */}
         <Link to="/posts" >Posts</Link>
         {/* <a href="/">Home</a>
         <a href="/about">About</a>
