1d2a8c48dd330a8ee76bdf30f516ca7434e51d21
najmul <mdnajmulislam10992@gmail.com>
2025-04-06 23:28:48 +0600

    use link for prevent reload

--- introduction-to-react-router/src/components/Header/Header.jsx
+++ introduction-to-react-router/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+import './Header.css'
 const Header = () => {
   return (
     <div>
       <h2>Navbar</h2>
       <nav>
-        <a href="/">Home</a>
+        <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/contact" >Contact Us</Link>
+        {/* <a href="/">Home</a>
         <a href="/about">About</a>
-        <a href="/contact">Contact Us</a>
+        <a href="/contact">Contact Us</a> */}
       </nav>
     </div>
   );
