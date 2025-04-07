ade984e2405485dbb33de34ddbf12ce5e0b33fe8
najmul <mdnajmulislam10992@gmail.com>
2025-04-07 23:46:58 +0600

    access field data

--- form-master/src/components/SimpleForm/SimpleForm.jsx
+++ form-master/src/components/SimpleForm/SimpleForm.jsx
@@ -3,12 +3,17 @@ const SimpleForm = () => {
     const handleSubmit = (e)=>{
         e.preventDefault();
         console.log('form submited');
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
 
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" /> <br />
+                <input type="text" name="name" /> <br />
+                <input type="email" name="email" /> <br />
+                <input type="text" name="phone" /> <br />
                 {/* <button>Submit</button> */}
                 <input type="submit" value="Submit" />
             </form>
