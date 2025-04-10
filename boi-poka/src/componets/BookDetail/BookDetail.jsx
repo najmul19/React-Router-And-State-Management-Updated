40caa4a990d73702d66b2fa19f12ffe10a1c486f
najmul <mdnajmulislam10992@gmail.com>
2025-04-10 21:41:23 +0600

    show data

--- boi-poka/src/componets/BookDetail/BookDetail.jsx
+++ boi-poka/src/componets/BookDetail/BookDetail.jsx
@@ -1,23 +1,29 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
-
 const BookDetail = () => {
-    // const param = useParams();
-    // console.log(param)
-    const {bookId} = useParams();
-    const data = useLoaderData();
-    
-    const id = parseInt(bookId)
-    const book = data.find(book=> book.bookId === id)
-    
-    console.log(bookId,book,id)
-    console.log(data)
+  // const param = useParams();
+  // console.log(param)
+  const { bookId } = useParams();
+  const data = useLoaderData();
 
-    return (
-        <div>
-            <h2>Book Detail: {bookId} </h2>
-        </div>
-    );
+  const id = parseInt(bookId);
+  const book = data.find((book) => book.bookId === id);
+
+  console.log(bookId, book, id);
+  console.log(data);
+
+  const { bookId: currentBookId, image } = book;
+
+  return (
+    <div className="my-10">
+      <h2>Book Detail: {bookId} </h2>
+      <img className="w-36" src={image} alt="" />
+      <br />
+
+      <button className="btn btn-outline mr-4">Read</button>
+      <button className="btn btn-accent">Wish List</button>
+    </div>
+  );
 };
 
 export default BookDetail;
