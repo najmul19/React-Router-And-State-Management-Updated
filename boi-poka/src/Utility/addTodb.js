0673d6ac09cfb72fecd3aec6e775c31a1249b41a
najmul <mdnajmulislam10992@gmail.com>
2025-04-10 22:00:12 +0600

    save mark as read to local storage

---
+++ boi-poka/src/Utility/addTodb.js
@@ -0,0 +1,24 @@
+const getStoredReadList=()=>{
+    // readlist
+    const storedListString = localStorage.getItem('read-list');
+    if(storedListString) {
+        const storedList = JSON.parse(storedListString);
+        return storedList;
+    } else {
+        return [];
+    }
+}
+
+const addToStoredReadList=(id)=>{
+ const storedList = getStoredReadList();
+ if(storedList.includes(id)){
+    // already exist
+    console.log(id, 'already exist in the read list');
+ } else {
+    storedList.push(id);
+    const storedListString = JSON.stringify(storedList);
+    localStorage.setItem('read-list',storedListString);
+ }
+}
+
+export {addToStoredReadList}
--- boi-poka/src/componets/BookDetail/BookDetail.jsx
+++ boi-poka/src/componets/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../Utility/addTodb";
 
 const BookDetail = () => {
   // const param = useParams();
@@ -14,14 +15,26 @@ const BookDetail = () => {
 
   const { bookId: currentBookId, image } = book;
 
+  const handleMarkAsRead=(id) =>{
+    /**
+     * 1. understand what to store or save: => bookId
+     * 2. where to store: database
+     * 3. array,list,collection:
+     * 4. check: if the book is already in the readlist
+     * 5. if not, then add the book to the list
+     * 6. if yess: do not add the book
+     */
+
+    addToStoredReadList(id);
+  }
   return (
     <div className="my-10">
       <h2>Book Detail: {bookId} </h2>
       <img className="w-36" src={image} alt="" />
       <br />
 
-      <button className="btn btn-outline mr-4">Read</button>
-      <button className="btn btn-accent">Wish List</button>
+      <button onClick={()=> handleMarkAsRead(bookId)} className="btn btn-outline mr-4">Read</button>
+      <button className="btn btn-accent">Add to Wish List</button>
     </div>
   );
 };
