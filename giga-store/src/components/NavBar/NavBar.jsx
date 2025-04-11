920a883243c600f9da1bced560d850b2b200781f
najmul <mdnajmulislam10992@gmail.com>
2025-04-12 01:24:58 +0600

    create a project name giga-store and setup tailwind daisyui react router and so on and also complete the navbar

---
+++ giga-store/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,81 @@
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className=" text-xl">Giga Store</a>
+      </div>
+      <div className="navbar-center md:hidden ">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <NavLink to="/home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/statistics">Statistics</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard">Dashboard</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-center hidden md:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            <NavLink to="/home">Home</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/statistics">Statistics</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end">
+        <div class="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+          <span class="badge badge-sm indicator-item">8</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ giga-store/src/main.jsx
@@ -0,0 +1,47 @@
+import { Children, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Root from './components/Root/Root.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx'
+import path from 'path'
+import Statistics from './components/Statistics/Statistics.jsx'
+import Dashboard from './components/Dashboard/Dashboard.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:  [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/home',
+        element: <Home></Home>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  }
+  
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
